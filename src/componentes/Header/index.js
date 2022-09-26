4967d70207f08751513525092b53b4d8315503ef
martincavigliasso <martincavigliasso@gmail.com>
2022-09-26 16:00:12 -0300

    front

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Nike from "../../images/Nike.jpg";
+
+export const Header = () => {
+    return(
+        
+           
+            <header>
+              <h1></h1>  
+             
+             <a href='header'> 
+             <div className='logo'>
+             <img src={Nike} alt="logo" width={150}></img>   
+             </div>
+             </a>
+              <ul>
+                <li>
+                <a href="#"> INICIO</a>
+                </li>
+                <li>
+                <a href="#">PRODUCTOS</a>
+                </li>
+              </ul>
+              <div className="cart">
+              <box-icon name= "cart"></box-icon>
+              <span className="item__total">0</span> 
+              </div>
+
+
+             </header>     
+            
+         
+    )
+}
