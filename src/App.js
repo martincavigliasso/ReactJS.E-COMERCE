ed2383111ef6c58e608665654afe9123e3c9cf22
martincavigliasso <martincavigliasso@gmail.com>
2022-10-13 16:31:49 -0300

    entrega

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-
-import Footer from './componentes/Footer/footer';
-import Navbar from './componentes/Header/Navbar';
+import NavBar from "./components/Header/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ItemDetailContainer from "./components/Main/ItemDetailContainer";
 import ItemListContainer from './componentes/Main/itemListContainer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartWidget from './componentes/Header/CartWidget';
 
 
 function App() {
     return (
-        <>
-            <Navbar />
-            <main>
-                <ItemListContainer />
-            </main>
-            <Footer />
-        </>
+        <BrowserRouter>
+          <NavBar />
+           <Routes>
+           <Route path='/' element={<ItemListContainer />} />
+             <Route path='/category/:categoryName' element={<ItemListContainer />} />
+             <Route path='/item/:id' element={<ItemDetailContainer />} />
+             <Route path='/cart' element={<CartWidget />} /> 
+           </Routes>
+        </BrowserRouter>
     );
 }
 
